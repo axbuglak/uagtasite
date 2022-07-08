d3f59a39685e5889f278183ec45b8f0e03b0cda2
Alexey-Buglak <alexbuglak2007@gmail.com>
2022-07-08 17:40:02 +0200

    upd main page

--- src/js/main.js
+++ src/js/main.js
@@ -111,15 +111,3 @@ setOnline();
         }
     }
 })()
-
-
-function scrollToGuid () {
-    const btn = document.querySelector('.section__btn');
-    const guid = document.querySelector('#guid');
-    if (btn && guid) {
-        btn.addEventListener('click', () => {
-            guid.scrollIntoView({ block: 'center', behavior: 'smooth' });
-        })
-    }
-}
-scrollToGuid()
