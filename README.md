08638d6f614649d5d9a685f726a757e3ade3ffbf
Alexey-Buglak <alexbuglak2007@gmail.com>
2022-08-26 00:06:09 +0200

    migrate ot vue3

--- README.md
+++
@@ -1,8 +0,0 @@
-## [UA GTA SITE](https://alexey-buglak.github.io/uagtasite/build/)
-
-### `npm i` 
-встановити пакети
-### `npm run dev` 
-запустити сервер
-### `npm run imgmin`
-мініфікація зображень
---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="menu">
+    <li class="menu__item">1</li>
+    <li class="menu__item">2</li>
+    <li class="menu__item">3</li>
+    <li class="menu__item">4</li>
+    <li class="menu__item">5</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" src="./scss/style.scss">
+
+</style>
