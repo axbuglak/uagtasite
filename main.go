ab1c27af84b2e78da44170915a8a9abb3a6b3a00
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-08-27 00:33:33 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
-package app
+package main
 
-import "log"
-
+import (
+	"context"
+	"net/http"
+	"time"
+	"log"
+)
 
 func main() {
 	srv := new(Server)
 	if err := srv.Run("8080"); err != nil {
 		log.Fatalf("Error while running server: %s", err.Error())
 	}
+}
+
+
+type Server struct {
+	httpServer *http.Server
+}
+
+
+func (s *Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr: ":" + port,
+		MaxHeaderBytes: 1 << 20, //1 M,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
 }
